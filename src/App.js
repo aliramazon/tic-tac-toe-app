ad91d536c05dc9307a284a730fa17319bf176d60
Ali Ramazon <mr.safarali@gmail.com>
2021-12-01 19:56:15 -0700

    Set up the game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div class="game">
+            <h1 className="game__title">Tic Tac Toe</h1>
+            <div className="game__settings">Settings</div>
+            <div className="game__board">Board</div>
+            <div className="game__results">Results</div>
+        </div>
+    );
+};
 
 export default App;
